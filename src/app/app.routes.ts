d80781e36ea594dbd16a8c35c08924fec8197dcd
Henrique Bispo <henriquefbispo.dev@gmail.com>
2025-09-24 19:47:49 -0300

    feat: implementa melhorias de rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component').then(m => m.LayoutComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./shared/components/home/home.component').then(m => m.HomeComponent),
+            },
+            {
+                path: 'transparencia',
+                loadComponent: () => import('./shared/components/transparency/transparency.component').then(m => m.TransparencyComponent),
+                data: {
+                    breadcrumb: 'Transparência'
+                }
+            },
+        ]
+    }
+];

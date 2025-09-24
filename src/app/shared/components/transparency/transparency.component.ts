00c86b8009b75c2fab48ded6ee96ad7e5a729151
Henrique Bispo <henriquefbispo.dev@gmail.com>
2025-09-24 19:49:12 -0300

    feat: adiciona estrutura inicial de componentes para exemplificação do layout

---
+++ src/app/shared/components/transparency/transparency.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transparency',
+  standalone: true,
+  imports: [],
+  templateUrl: './transparency.component.html',
+  styleUrl: './transparency.component.scss'
+})
+export class TransparencyComponent {
+
+}

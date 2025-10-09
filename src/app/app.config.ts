7bb9a3f976e4c6e5b23008db375d8f5581ca5ed2
Henrique Bispo <henriquefbispo.dev@gmail.com>
2025-10-08 22:19:24 -0300

    feat: adiciona configs para data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withFetch())

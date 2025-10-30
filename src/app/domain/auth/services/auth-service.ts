c50bee70ff9e01473de52f111176e4ab529b2913
Henrique Bispo <henriquefbispo.dev@gmail.com>
2025-10-30 19:24:01 -0300

    feat: implementa cookies

--- src/app/domain/auth/services/auth-service.ts
+++ src/app/domain/auth/services/auth-service.ts
@@ -5,6 +5,7 @@ import { LoginRequest } from '../models/login-request';
 import { Observable, tap } from 'rxjs';
 import { LoginResponse } from '../models/login-response';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +14,35 @@ export class AuthService {
 
   private readonly API_URL = `${environment.apiUrl}/auth`;
 
+  private cookieService = inject(CookieService);
   private httpClient = inject(HttpClient);
   private router = inject(Router);
 
   login(data: LoginRequest): Observable<LoginResponse> {
     return this.httpClient.post<LoginResponse>(`${this.API_URL}/login`, data).pipe(
       tap((response: LoginResponse) => {
-        localStorage.setItem('token', response.token);
+        this.cookieService.set(
+          'token',             
+          response.token,      
+          7,                   
+          '/',                
+          undefined,                
+          false,               
+          'Lax'                
+        );
         this.router.navigate(['/']);
       })
     );
   }
 
   logout(): string {
-    localStorage.removeItem('token');
+    this.cookieService.delete('token');
     return 'login';
+  }
+
+  getToken(): string | null {
+    const token = this.cookieService.get('token');
+    return token ? token : null;
   }
 
 }
--- src/app/shared/guards/auth/auth-guard.ts
+++ src/app/shared/guards/auth/auth-guard.ts
@@ -1,11 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { AuthService } from '../../../domain/auth/services/auth-service';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const router = inject(Router);
-  const token = localStorage.getItem('token');
+  const authService = inject(AuthService);
+
   const isLoginPage = state.url === '/login';
+  const token = authService.getToken();
 
   if (!token && !isLoginPage) {
     return router.parseUrl('/login');
